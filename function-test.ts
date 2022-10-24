95cb8b71e17692fbfe6bc39c213cdabed314a7ab
dafaarmanto <randikadafa07@gmail.com>
2022-10-24 14:52:03 +0700

    Add overloading-function.ts

--- function-test.ts
+++ function-test.ts
@@ -1,6 +1,13 @@
 import addNumbers from './functions';
 import { arrayMutate, addFive } from './func-and-func';
+import { parseCoordinate } from './overloading-function';
 
 console.log(addNumbers(5, 5));
+
 console.log(arrayMutate([1, 2, 3], (v) => v * 30));
+
 console.log(addFive(55));
+
+console.log(parseCoordinate(10, 20));
+console.log(parseCoordinate({ x: 52, y: 35 }));
+console.log(parseCoordinate('x:12,y:22'));
---
+++ overloading-function.ts
@@ -0,0 +1,32 @@
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+export function parseCoordinate(str: string): Coordinate;
+export function parseCoordinate(obj: Coordinate): Coordinate;
+export function parseCoordinate(x: number, y: number): Coordinate;
+export function parseCoordinate(arg1: unknown, arg2?: unknown): Coordinate {
+  let coord: Coordinate = {
+    x: 0,
+    y: 0,
+  };
+
+  if (typeof arg1 === 'object') {
+    coord = {
+      ...(arg1 as Coordinate),
+    };
+  } else if (typeof arg1 === 'string') {
+    (arg1 as string).split(',').forEach((str) => {
+      const [key, val] = str.split(':');
+      coord[key as 'x' | 'y'] = parseInt(val, 10);
+    });
+  } else {
+    coord = {
+      x: arg1 as number,
+      y: arg2 as number,
+    };
+  }
+
+  return coord;
+}
